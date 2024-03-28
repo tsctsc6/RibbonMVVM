e0ce831c67ca1f92d36e179b673eb22178f92119
tsctsc6@126.com <tsctsc6@126.com>
2024-03-28 17:28:12 +0800

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+# Ribbon_MVVM
---
+++ Ribbon_MVVM/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Diagnostics;
+using System.Windows;
+
+namespace Ribbon_MVVM
+{
+    public partial class MainWindowViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string inputText = "";
+
+        [RelayCommand]
+        private async Task SomeTaskAsync()
+        {
+            await Task.Delay(500);
+            Debug.WriteLine(InputText);
+            MessageBox.Show(InputText);
+        }
+    }
+}
