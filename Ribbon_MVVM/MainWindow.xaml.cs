1fddc960fdeb0672284a11bfa4bcd29d625b8977
tsctsc6@126.com <tsctsc6@126.com>
2024-03-29 12:43:43 +0800

    debug

--- Ribbon_MVVM/MainWindow.xaml.cs
+++ Ribbon_MVVM/MainWindow.xaml.cs
@@ -1,11 +1,13 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Ribbon;
 
 namespace Ribbon_MVVM
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
